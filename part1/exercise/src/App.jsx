e0870b795d214ba6c46079f724b12f70780a51a4
Angel Diaz <angeldiaz.hrn@gmail.com>
2025-01-08 20:45:52 -0600

    exercise 1.5

--- part1/exercise/src/App.jsx
+++ part1/exercise/src/App.jsx
@@ -1,28 +1,30 @@
 /* eslint-disable react/jsx-key */
 /* eslint-disable react/prop-types */
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
       <Header course = {course} />
-      <Content parts = {parts} />
-      <Total parts = {parts} />
-      </div>
+      <Content course = {course}/>
+      <Total course = {course} />
+    </div>
   )
 }
 //First idea: <Total sum = {parts.reduce((sum, part) => sum + part.exercises, 0)} />
@@ -31,7 +33,7 @@ const Header = (props) => {
   return (
     <>
       <h1>
-        {props.course}
+        {props.course.name}
       </h1>
     </>
   )
@@ -40,7 +42,7 @@ const Header = (props) => {
 const Content = (props) => {
   return(
     <>
-      {props.parts.map(valor => <Part part = {valor.name} exercises = {valor.exercises}/>)}
+      {props.course.parts.map(valor => <Part part = {valor.name} exercises = {valor.exercises}/>)}
     </>
   )
 };
@@ -55,12 +57,14 @@ const Part = (props) => {
 }
 
 const Total = (props) => {
-  const total = props.parts.reduce((sum, part) => sum + part.exercises, 0)
+  const total = props.course.parts.reduce((sum, part) => sum + part.exercises, 0)
   //Method: https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
   return (
+    <>
     <p>
       Number of exercises {total}
     </p>
+    </>
   )
 }
 
